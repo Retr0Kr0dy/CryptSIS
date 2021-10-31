51468212aae4ab87226fb253ee82be3adf82b977
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2021-10-31 23:20:52 +0100

    Create Crypsis-2_3.py

---
+++ Crypsis-2_3.py
@@ -0,0 +1,53 @@
+__author__ = 'RetR0'
+
+from hashlib import sha256
+from os import urandom
+import os
+import hashlib
+import blowfish
+import sys
+
+to_do = input ("type [e] (for encrypt) or type [d] (for decrypt) :")
+
+if to_do == "d":
+    text = input ("Enter the name of the file to decrypt :")
+    output = input ("Enter the output file name :")
+    keyfile = input("Enter the name of the key file to use : ")
+    try:
+        with open (keyfile, 'rb') as f_key:
+            rawkey = f_key.read()
+            iv = bytes(rawkey[:8])
+            key = bytes(rawkey [8:])
+    except EnvironmentError:
+        print("Error : key file not found")
+        exit(-1)
+
+elif to_do == "e":
+    text = input ("Enter the name of the file to crypt :")
+    output = input ("Enter the output file name :")
+    iv = os.urandom(8)                
+    key = os.urandom(56)
+    keyfile = input("Enter the name of the key file to create : ")
+    try :
+        with open (keyfile, 'wb') as f_keyfile:
+            f_keyfile.write(iv + key)
+    except EnvironmentError:
+        print ("IO Error")
+        exit(-1)
+
+hashnsalt = blowfish.Cipher(key)
+
+with open (text, 'rb') as f_file_to_crypt:
+    text_block = f_file_to_crypt.read()
+
+match to_do:   
+    case "e":
+        data_result = b"".join(hashnsalt.encrypt_cfb(text_block, iv))
+    case " d":
+        data_result = b"".join(hashnsalt.decrypt_cfb(text_block, iv))
+
+with open (output, 'wb') as f_output:
+     f_output.write(data_result)
+
+
+#version 2.3
