518c8cbe9f2971a92b9343c985bdb553ee64e8a4
Kr0dÿ <91196264+Retr0Kr0dy@users.noreply.github.com>
2021-11-16 15:41:46 +0100

    Update CryptSIS_F-app.1.0.1.py

--- Full_version/CryptSIS_F-app.1.0.1.py
+++ Full_version/CryptSIS_F-app.1.0.1.py
@@ -1,7 +1,11 @@
 __author__ = 'RetR0'
 
 from hashlib import sha256
+from os import urandom
 import hashlib
+import os
+import blowfish
+import sys
 
 
 print (" ___________________________________________________________\n*************************************************************")
@@ -12,11 +16,12 @@ print (""" ██████╗██████╗ ██╗   ██╗█�
 ╚██████╗██║  ██║   ██║   ██║        ██║   ███████║██║███████║
  ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝        ╚═╝   ╚══════╝╚═╝╚══════╝""")
 print ("*************************************************************\n ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ͞ ")
-print ("Encrypt/Decrpyt Using different Ciphers, Version F-exe.1.0.1")
+print ("Encrypt/Decrpyt Using different Ciphers, Version F-app.1.0.1")
+
+
 
 def main ():
     to_do = input ("\n\n1 - SHA256/utf-8\n2 - Blowfish\n3 - AES (cryptodome)\n\nPlease enter a number : ")
-
     if to_do == "1":
         verion1cipher()
     
@@ -25,14 +30,18 @@ def main ():
 
     if to_do == "3":
         version3cipher()
+    
+
 
 
 def verion1cipher ():
+
     print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n**********************SHA256/utf-8***************************")
     print ("*************************************************************")
     print ("\n\n\n╔═══════════════════════════════════════════════════════════╗")
     print ("║ Two way encryption, key isn't save in a file, you got to  ║ \n║ type it in raw text whenever you want to use this cipher. ║")
     print ("╚═══════════════════════════════════════════════════════════╝\n")
+    
     i = 0
     file_to_crypt = input ("Enter the name of the file to encrypt/decrypt :")
     output = input ("Enter the output file name :")
@@ -58,12 +67,73 @@ def verion1cipher ():
     #version 1.4 (modified)
 
 
+
 def version2cipher ():
+
     print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n************************Blowfish*****************************")
     print ("*************************************************************")
     print ("\n\n\n╔═══════════════════════════════════════════════════════════╗")
     print ("║ Two way encryption, multi-usage key (re-use same key).    ║ \n║ No authentification.                                      ║")
     print ("╚═══════════════════════════════════════════════════════════╝\n")
+
+    to_do = input ("1 - Encrpytion\n2 - Decryption\nPlease enter a number : ")
+    if to_do == "1":
+        text = input ("Enter the name of the file to decrypt :")
+        output = input ("Enter the output file name :")
+        keyfile = input("Enter the name of the key file to use : ")
+        try:
+            with open (keyfile, 'rb') as f_key:
+                rawkey = f_key.read()
+                iv = bytes(rawkey[:8])
+                key = bytes(rawkey [8:])
+        except EnvironmentError:
+            print("Error : key file not found")
+            exit(-1)
+    elif to_do == "2":
+        text = input ("Enter the name of the file to crypt :")
+        output = input ("Enter the output file name :")
+        keyfile = input ("Import a key file to use or leave empty to create one :")
+        if len(keyfile) > 0:
+            iv = os.urandom(8)                
+            key = os.urandom(56)
+            keyfile = keyfile
+            try :
+                with open (keyfile, 'rb') as f_keyfile:
+                    rawkey = f_keyfile.read()
+                iv = bytes(rawkey[:8])
+                key = bytes(rawkey [8:])
+            except EnvironmentError:
+                print ("I/O Error")
+                exit(-1)        
+        elif len(keyfile)== 0 :
+            iv = os.urandom(8)                
+            key = os.urandom(56)
+            keyfile = input("Enter the name of the key file to create : ")
+            try:
+                with open (keyfile, 'wb') as f_keyfile:
+                    f_keyfile.write(iv + key)
+            except EnvironmentError:
+                print("Error : key file not found")
+                exit(-1)
+    else:
+        print ("Error : invalid Option")
+    hashnsalt = blowfish.Cipher(key)
+
+    with open (text, 'rb') as f_file_to_crypt:
+        text_block = f_file_to_crypt.read()
+
+    match to_do:   
+        case "1":
+            data_result = b"".join(hashnsalt.encrypt_cfb(text_block, iv))
+        case "2":
+            data_result = b"".join(hashnsalt.decrypt_cfb(text_block, iv))
+
+    with open (output, 'wb') as f_output:
+        f_output.write(data_result)
+
+    #version 2.6 (modified)
+
+
 
 def version3cipher ():
     print ("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n********************AES (cryptodome)*************************")
@@ -72,7 +142,9 @@ def version3cipher ():
     print ("║ Two way encryption, multi-usage key (re-use same key).    ║ \n║ No authentification.                                      ║")
     print ("╚═══════════════════════════════════════════════════════════╝\n")
 
+
+
 if __name__ == "__main__":
     main()
 
-# Version F-exe.1.0.1
+# Version F-app.1.0.1
